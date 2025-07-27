e0d7dc2b2e20ac34a586d87233583920e3ab22f4
Thais Machado <thaiscristianemachado@hotmail.com>
2025-07-27 17:10:09 -0300

    Banco de dados criado

--- Models/Laboratorio.cs
+++ Models/Laboratorio.cs
@@ -7,14 +7,15 @@ public class Laboratorio
     {
         Professores = new Collection<Professor>();
         Reservas = new Collection<Reserva>();
+        Turmas = new Collection<Turma>();
     }
 
     public int LaboratorioId { get; set; }
     public string Nome { get; set; } = string.Empty;
     public int Capacidade { get; set; } = 20;
     public Collection<Professor> Professores { get; set; }
-    public Collection <Reserva> Reservas { get; internal set; }
-    public object Turmas { get;  set; }
+    public Collection <Reserva> Reservas { get;  set; }
+    public Collection <Turma> Turmas { get;  set; }
 
     // Remover se não quiser navegação inversa
     // public ICollection<Reserva> Reservas { get; set; }
---
+++ Models/ProfessorTurma.cs
@@ -0,0 +1,13 @@
+﻿using ReservaDeLaboratorioContext.Models;
+
+namespace APIResevaDeLaboratorio.Models;
+
+public class ProfessorTurma
+{
+    public int ProfessorId { get; set; }
+    public Professor Professor { get; set; } = null!;
+    public int TurmaId { get; set; }
+    public Turma Turma { get; set; } = null!;
+    // Outros campos adicionais, se necessário
+
+}
